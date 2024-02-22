e0a2aa8393ed90c4bac1891936c5e6569b7b6036
João Barbosa <contato.jsbarbosa@gmail.com>
2024-02-21 21:45:14 -0300

    Iniciando o projeto

---
+++ backend/src/main/java/br/com/joaosbarbosa/backend/BackendWolfJbEcomHubApplication.java
@@ -0,0 +1,13 @@
+package br.com.joaosbarbosa.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendWolfJbEcomHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendWolfJbEcomHubApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/joaosbarbosa/backend/BackendWolfJbEcomHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaosbarbosa.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendWolfJbEcomHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
