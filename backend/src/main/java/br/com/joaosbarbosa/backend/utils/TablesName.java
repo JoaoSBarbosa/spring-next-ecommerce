c5b444d6e6ae1ca539281659906cfb32759e04e1
João Barbosa <contato.jsbarbosa@gmail.com>
2024-03-04 21:37:18 -0300

    Entidade e Repository para Categoria

--- backend/src/main/java/br/com/joaosbarbosa/backend/utils/TablesName.java
+++ backend/src/main/java/br/com/joaosbarbosa/backend/utils/TablesName.java
@@ -6,4 +6,5 @@ public abstract class TablesName {
     public static final String TABLE_ESTADOS = "\"tb_estado\"";
     public static final String TABLE_CIDADES = "\"tb_cidade\"";
     public static final String TABLE_MARCAS = "\"tb_marca\"";
+    public static final String TABLE_CATEGORIAS = "\"tb_categoria\"";
 }
