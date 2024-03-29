c82929ffa91e659399d8fa5e6fb5aa5ebab82f8b
João Barbosa <contato.jsbarbosa@gmail.com>
2024-03-29 09:54:46 -0300

    Implementado página home com thymeleaf

--- backend/src/main/java/br/com/joaosbarbosa/backend/controllers/CategoryController.java
+++ backend/src/main/java/br/com/joaosbarbosa/backend/controllers/CategoryController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/{categories}")
+@RequestMapping("/categories")
 public class CategoryController {
 
     @Autowired
